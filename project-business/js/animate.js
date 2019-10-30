505cc1e922bda7d1353552d293ecfaa049699034
xiongerya <xiongwendi5@163.com>
2019-10-30 16:32:13 +0800

    new images

--- project-business/js/animate.js
+++ project-business/js/animate.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function(){
     // banner部分焦点图轮播动画
     // banner-side焦点图轮播动画效果
-    // tab栏切换效果 && icons 鼠标移动动图
+    // logIn tab栏切换效果 && icons 鼠标移动效果
+    //main秒杀倒计时效果
 })
