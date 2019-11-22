f9e04de9af09fce2e3284183bba1319831cba9a0
xiongerya <xiongwendi5@163.com>
2019-11-22 16:50:22 +0800

    bolg page

--- js/blog.js
+++ js/blog.js
@@ -1,28 +1,28 @@
 window.addEventListener("load", function(){
     // h3 点击显示/隐藏下拉菜单，并改变icon
     // tag下拉菜单
-    let lis = document.querySelectorAll(".system > li")
+    let system = document.querySelector(".system"),
+        lis = document.querySelectorAll(".system > li"),
         len = lis.length;
+   
+    function showOrHide(obj){
+        let title = obj.querySelector(".title"),
+            ul = obj.querySelector("ul");
 
-    for(let i = 0; i < len ; i++){
         let flag = true;
-        function showSub(obj){
-            let sub = obj.querySelector(".list"),
-                icon = obj.querySelector(".title > i");
-            if(flag){
-                sub.style.display = "block";
-                // icon.className = "jianhao";
-            }else{
-                sub.style.display = "none";
-                // icon.className = "";
-            }
+        function change(){
+             ul.style.display = flag ? "block" : "none";
             flag = !flag;
         }
-        let title = lis[i].querySelector(".title");
-        title.addEventListener("click", function(e){
-            showSub(lis[i]);
-            e.stopPropagation();
+
+        title.addEventListener("click", function(){
+           change();
         })
     }
 
+    for(let i = 0; i < len; i++){
+        showOrHide(lis[i]);
+        let sub_lis = lis[i].querySelectorAll(".list > li");
+        console.log(sub_lis);
+    }
 })
