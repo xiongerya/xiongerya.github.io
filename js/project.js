08f1c0a58ee3e348fdf20b49cfca4eeee457d19d
xiongerya <xiongwendi5@163.com>
2019-12-07 22:31:31 +0800

    images size & project change

--- js/project.js
+++ js/project.js
@@ -1,18 +1,21 @@
 window.addEventListener("load", function(){
-    // 点击expand展开全部信息
+    // 点击wrap展开全部信息
     let wraps = document.querySelectorAll(".wrap"),
         spans = document.querySelectorAll(".expand span"),
         len = wraps.length;
 
     for(let i = 0; i < len; i++){
-        spans[i].index = i;
+        wraps[i].index = i;
     }
     for(let i = 0; i < len; i++){
         let flag = true;
-        spans[i].addEventListener("click", function(){
-            wraps[this.index].style.height = flag ? "100%" : "300px";
-            this.innerHTML = flag ? "收起" : "展开";
+        wraps[i].addEventListener("click", function(){
+            this.style.height = flag ? "100%" : "300px";
+            spans[this.index].innerHTML = flag ? "收起" : "展开";
             flag = !flag;
+        })
+        wraps[i].querySelector("figure").addEventListener("click", function(e){
+            e.stopPropagation();
         })
     }
 })
