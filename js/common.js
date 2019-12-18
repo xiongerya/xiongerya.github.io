d6a2d73002f6ea95bbfaca30ea9f624c190652cc
xiongerya <xiongwendi5@163.com>
2019-12-18 14:22:22 +0800

    fixed some bugs

--- js/common.js
+++ js/common.js
@@ -13,10 +13,10 @@ window.addEventListener("load", function(){
     }
 
     let nav = document.querySelector("nav");
-    add(nav, "https://xiongerya.github.io/_includes/nav.html", "html")
+    add(nav, "_includes/nav.html", "html")
 
     let footer = document.querySelector("footer");
-    add(footer, "https://xiongerya.github.io/_includes/footer.html", "html")
+    add(footer, "_includes/footer.html", "html")
 
      // 为所有.blank的锚点添加target="_blank"属性
      let blanks = document.querySelectorAll("a.blank");
