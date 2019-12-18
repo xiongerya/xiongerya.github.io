b2f02c61033750cc9f8bd6502487a46fcb0f92c0
xiongerya <xiongwendi5@163.com>
2019-12-18 13:54:38 +0800

    利用Ajax动态加载nav/footer

--- js/common.js
+++ js/common.js
@@ -1,7 +1,27 @@
 window.addEventListener("load", function(){
-    // 为所有.blank的锚点添加target="_blank"属性
-    let blanks = document.querySelectorAll("a.blank");
-    for(let i = 0, len = blanks.length; i < len; i++){
-        blanks[i].target = "_blank";
+    // 利用Ajax加载nav/header/footer部分
+    function add(obj, url, type){
+        let request = new XMLHttpRequest();
+        request.open('get', url);
+        request.responseType = type;
+
+        request.onload = function(){
+            obj.innerHTML = request.response;
+        }
+
+        request.send();
     }
+
+    let nav = document.querySelector("nav");
+    add(nav, "_includes/nav.html", "html")
+
+    let footer = document.querySelector("footer");
+    add(footer, "_includes/footer.html", "html")
+
+     // 为所有.blank的锚点添加target="_blank"属性
+     let blanks = document.querySelectorAll("a.blank");
+     for(let i = 0, len = blanks.length; i < len; i++){
+         blanks[i].target = "_blank";
+     }
+ 
 })
