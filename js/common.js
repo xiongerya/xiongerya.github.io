4275e1f917444ad600d6714da53988d6a69510d0
xiongerya <xiongwendi5@163.com>
2019-12-19 15:52:53 +0800

    loading加载动画

--- js/common.js
+++ js/common.js
@@ -18,6 +18,14 @@ window.addEventListener("load", function(){
     let footer = document.querySelector("footer");
     add(footer, "common_footer.html", "html")
 
+    // loading动画效果加载
+    let load = document.querySelector("#load");
+    add(load, "common_load.html", "html")
+    // 页面加载完成之后隐藏
+    if(document.readyState = "400"){
+        load.style.display = "none";
+    }
+
      // 为所有.blank的锚点添加target="_blank"属性
      let blanks = document.querySelectorAll("a.blank");
      for(let i = 0, len = blanks.length; i < len; i++){
