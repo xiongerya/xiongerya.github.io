2c24b11dad8019310f6787e7f0b8248ce51d860b
xiongerya <xiongwendi5@163.com>
2020-03-01 20:05:19 +0800

    set website structure by bootstrap

--- project-music/js/vue.js
+++ project-music/js/vue.js
@@ -1,7 +1,7 @@
 let music = new Vue({
     el: "#music",
     data: {
-        list: ["隐形的翅膀", "青花瓷", "左手指月", "summertime"]
+        list: ["隐形的翅膀", "青花瓷", "左手指月", "summertime", "不染", "勇气", "告白气球", "刀马旦", "欧若拉", "莫斯科不相信眼泪", "Faded"]
     },
     methods: {
 
