9bb33c6c636d1987a4aabf3d1c240728dbc559da
xiongerya <xiongwendi5@163.com>
2020-05-02 16:40:39 +0800

    rename vue.js to app.js

--- project-music/js/app.js
+++ project-music/js/app.js
@@ -1,5 +1,5 @@
 let app = new Vue({
-    el: "#wrap",
+    el: "#app",
     data: {
         keyword: "",
         songs: [],
@@ -7,74 +7,82 @@ let app = new Vue({
         picUrl: "./images/music.png",
         mvUrl: "",
         comments: [],
-        music: false,
-        mv: false
+        music: "",
+        isPlay: false
     },
     methods: {
         getSongs(){
             let that = this;
             axios
             .get("https://autumnfish.cn/search?keywords=" + that.keyword)
-            .then(response => {
-                let result =  response.data.result.songs;
-                result.map(x => {
+            .then(
+                response => response.data.result.songs
+            )
+            .then(value => {
+                let result =  value.map(x => {
+                    let song = {name: x.name, id: x.id};
                     // 获取song的musicUrl
                     axios
                     .get("https://autumnfish.cn/song/url?id=" + x.id)
                     .then(response => {
-                        x.musicUrl = response.data.data[0].url;
+                        song.musicUrl = response.data.data[0].url;
                     })
                     .catch(err => console.log("出错啦：" + err));
                     // 获取song的imgUrl
                     axios
-                    .get("https://autumnfish.cn/song/detail?ids=" + x.id)
+                    .get("https://autumnfish.cn/song/detail?ids=" + song.id)
                     .then(response => {
-                        x.picUrl = response.data.songs[0].al.picUrl;
+                        song.picUrl = response.data.songs[0].al.picUrl;
                     })
                     .catch(err => console.log("出错啦：" + err));
                     // 获取song的mvUrl
                     axios
-                    .get("https://autumnfish.cn/mv/url?id=" + x.id)
+                    .get("https://autumnfish.cn/mv/url?id=" + song.id)
                     .then(response => {
-                        x.mvUrl = response.data.data.url;
+                        song.mvUrl = response.data.data.url;
                     })
                     .catch(err => console.log("出错啦：" + err));
                     // 获取song的comments
                     axios
-                    .get("https://autumnfish.cn/comment/hot?type=0&id=" + x.id)
+                    .get("https://autumnfish.cn/comment/hot?type=0&id=" + song.id)
                     .then(response => {
-                        x.comments = response.data.hotComments;
+                        song.comments = response.data.hotComments;
                     })
                     .catch(err => console.log("出错啦：" + err));
+                    return song;
                 });
-                setTimeout(() => that.songs = result, 500)
+                return result;
+            })
+            .then(value => {
+                setTimeout(() => that.songs = value, 800)
+                // that.songs = value;
             })
             .catch(err => console.log("出错啦：" + err));
         },
-        playMusic(obj){
+        playMusic(obj){  
+            this.music = obj.name;
             this.musicUrl = obj.musicUrl;
             this.picUrl = obj.picUrl ? obj.picUrl : this.picUrl;
             this.comments = obj.comments;
-            // console.log(obj.name)
-            // console.log(obj.id)
-            // console.log(obj.musicUrl);
-            // console.log(obj.picUrl)
+            this.isPlay = true;
+        },
+        change(){
+            this.isPlay = !this.isPlay;
+            this.isPlay ? this.$refs.audio.play() : this.$refs.audio.pause();
         },
         playMv(str){
             this.mvUrl = str;
-            // console.log(this.mvUrl)
+            this.$refs.audio.pause();
         },
-        isPlay(){
-            this.music = true;
-        },
-        isPause(){
-            this.music = false;
+        stopMv(){
+            this.mvUrl = "";
         }
     }
 })
 
-setTimeout(() => {
-    app.keyword = "杨幂";
-    app.getSongs();
-    app.keyword = "";
-}, 0)
+
+
+//开始前执行getSongs()，构建一份歌曲目录
+app.keyword = "杨幂";
+app.getSongs();
+app.keyword = "";
